033d984d1dad5e22665ef26f90b4956e0f8224f7
Ehden Sinai <ehden.sinai@contrastsecurity.com>
2016-02-26 15:49:28 -0500

    Add dotfiles for eslint and jsbeautify and fix formatting

--- vulnerabilities/command_injection/index.js
+++ vulnerabilities/command_injection/index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var childProcess = require('child_process');
 
 module.exports = (function() {
-    'use strict';
-    var api = express.Router();
+	'use strict';
+	var api = express.Router();
 
-    api.get('/', function(req, res) {
+	api.get('/', function(req, res) {
 		res.render('../vulnerabilities/command_injection/views/index');
-    });
+	});
 
-    api.get('/childprocess_exec', function(req, res) {
+	api.get('/childprocess_exec', function(req, res) {
 		// this should be tainted
 		var path = req.query.user_path;
-	
+
 		var ls = 'ls -l ';
-	
+
 		// propagation from path with concat of ls to new var cmd
 		var cmd = ls + path;
 
@@ -22,22 +22,22 @@ module.exports = (function() {
 		childProcess.exec(cmd, function(err, data) {
 			res.send('<xmp>' + data);
 		});
-    });
+	});
 
 	api.get('/param_test/:input', function(req, res) {
 		res.set('X-XSS-Protection', '0'); // disable browser xss protection for chrome
-		var output = "<html>param: " + req.params.input + "</html>"
-		// this should trigger XSS 
+		var output = '<html>param: ' + req.params.input + '</html>';
+			// this should trigger XSS 
 		res.send(output);
 	});
 
-	api.post('/xss_post', function (req, res, next) {
+	api.post('/xss_post', function(req, res, next) {
 		res.set('X-XSS-Protection', '0'); // disable browser xss protection for chrome
 		var input = req.body.email;
-		var output = "<html>e-mail: " + input + "</html>"
+		var output = '<html>e-mail: ' + input + '</html>';
 		console.log(output.__contrastProperties);
 		res.send(output);
 	});
-	
-    return api;
+
+	return api;
 })();
