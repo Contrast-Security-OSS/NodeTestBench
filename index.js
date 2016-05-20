27c16a5b99a0386739e43608cc57dd37ed095979
Matt Austin <matt@m-austin.com>
2016-05-19 23:28:57 -0700

    Added MySQL injection test.

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ app.use('/assets', express.static('public'));
 //app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-
+app.set('views', __dirname+'/views');
 app.set('view engine', 'ejs');
 
 app.use('/xss_test', require('./vulnerabilities/xss/'));
+app.use('/sqli', require('./vulnerabilities/sqli/'));
 app.use('/command_injection', require('./vulnerabilities/command_injection/'));
 app.use('/unsafe_eval', require('./vulnerabilities/unsafe_eval/'));
 app.use('/crypto', require('./vulnerabilities/crypto/'));
---
+++ vulnerabilities/sqli/index.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var hooker = require('hooker');
+var mysql = require('mysql');
+
+var util = require('util');
+
+// mock the sql query so the app does not require a database connection
+hooker.hook(require("mysql/lib/Connection").prototype, "query", {
+	pre: function() {
+		console.log(arguments[0]);
+	},
+	post: function(result, sql, cb) {
+		cb(null,[
+				{query: sql}
+			]
+		);
+	}
+});
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root'
+});
+
+// pretend we already connected
+connection._connectCalled = true;
+
+connection.connect();
+
+
+module.exports = (function () {
+    'use strict';
+    var api = express.Router();
+
+    api.get('/', function (req, res) {
+		res.render(__dirname + '/views/index');
+    });
+
+    api.get('/mysql', function (req, res) {
+		connection.query('SELECT "'+req.query.name+'" as "test";',
+			function (error, rows, fields) {
+				res.send('The solution is: ' + util.inspect(rows));
+			}
+		);
+    });
+
+    return api;
+})();
