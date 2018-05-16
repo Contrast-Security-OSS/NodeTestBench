7ba89533749706c43dc2e9ae94ae3cf5a49c56d3
Alex Boyd <alex.boyd@contrastsecurity.com>
2018-05-16 16:51:17 -0400

    updated app name

--- newrelic.js
+++ newrelic.js
@@ -9,7 +9,7 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: ['My Application'],
+  app_name: ['Node-Test-Bench-NewRelic'],
   /**
    * Your New Relic license key.
    */
